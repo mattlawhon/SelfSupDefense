226652e01c45fc788c9a958cc306de1ebdf7f305
mattlawhon <matt.lawhon@gmail.com>
2022-03-05 14:03:04 -0500

    test

--- cifar10_defense.py
+++ cifar10_defense.py
@@ -57,7 +57,7 @@ class Batches():
     def __len__(self):
         return len(self.dataloader)
 
-def attack_constrastive_Mhead(model, model_ssl, rot, cont, scripted_transforms, criterion, X, y, epsilon, alpha, attack_iters, restarts,
+def attack_constrastive_Mhead1(model, model_ssl, rot, cont, scripted_transforms, criterion, X, y, epsilon, alpha, attack_iters, restarts,
                norm, early_stop=False,
                mixup=False, y_a=None, y_b=None, lam=None, Ltype=None, reverse=False, n_views=2):
     """Reverse algorithm that optimize the SSL loss via PGD"""
@@ -116,6 +116,58 @@ def attack_constrastive_Mhead(model, model_ssl, rot, cont, scripted_transforms, 
         delta2.grad.zero_()
         deltasu.grad.zero_()
     max_delta = delta1.detach()
+    return max_delta
+def attack_constrastive_Mhead(model, model_ssl, rot, cont, scripted_transforms, criterion, X, y, epsilon, alpha, attack_iters, restarts,
+               norm, early_stop=False,
+               mixup=False, y_a=None, y_b=None, lam=None, Ltype=None, reverse=False, n_views=2):
+    """Reverse algorithm that optimize the SSL loss via PGD"""
+    if torch.cuda.is_available():
+        delta = torch.zeros_like(X).cuda()
+    else:
+        delta = torch.zeros_like(X)
+    if norm == "l_inf":
+        delta.uniform_(-epsilon, epsilon)
+    elif norm == "l_2":
+        delta.normal_()
+        d_flat = delta.view(delta.size(0),-1)
+        n = d_flat.norm(p=2,dim=1).view(delta.size(0),1,1,1)
+        r = torch.zeros_like(n).uniform_(0, 1)
+        delta *= r/n*epsilon
+    elif norm == 'l_1':
+        pass
+    else:
+        raise ValueError
+    delta = clamp(delta, lower_limit-X, upper_limit-X)
+    delta.requires_grad = True
+    for _ in range(attack_iters):
+
+        new_x = X + delta
+        # import pdb; pdb.set_trace()
+
+        # TODO: here the neg sample is fixed, we can also try random neg sample to enlarge and diversify
+        loss = -calculate_contrastive_Mhead_loss(new_x, scripted_transforms, model, criterion,
+                                                     model_ssl, rot, cont, no_grad=False, n_views=n_views)
+        loss.backward()
+        grad = delta.grad.detach()
+
+        d = delta
+        g = grad
+        x = X
+        if norm == "l_inf":
+            d = torch.clamp(d + alpha * torch.sign(g), min=-epsilon, max=epsilon)
+        elif norm == "l_2":
+            g_norm = torch.norm(g.view(g.shape[0],-1),dim=1).view(-1,1,1,1)
+            scaled_g = g/(g_norm + 1e-10)
+            d = (d + scaled_g*alpha).view(d.size(0),-1).renorm(p=2,dim=0,maxnorm=epsilon).view_as(d)
+        elif norm == "l_1":
+            g_norm = torch.sum(torch.abs(g.view(g.shape[0], -1)), dim=1).view(-1, 1, 1, 1)
+            scaled_g = g / (g_norm + 1e-10)
+            d = (d + scaled_g * alpha).view(d.size(0), -1).renorm(p=1, dim=0, maxnorm=epsilon).view_as(d)
+
+        d = clamp(d, lower_limit - x, upper_limit - x)
+        delta.data = d
+        delta.grad.zero_()
+    max_delta = delta.detach()
     return max_delta
 
 
